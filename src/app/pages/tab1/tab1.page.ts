fecca1a01b485a8b1a0d4f8a57ba00b436f9ecfc
Alex Ballera <alexballera@gmail.com>
2019-11-13 10:21:46 -0300

    Detalles en pantalla de agregar

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -16,7 +16,6 @@ export class Tab1Page {
   }
 
   async agregarLista() {
-    // this.router.navigateByUrl('/tabs/tab1/agregar');
     const alert = await this.alertController.create({
       header: 'Nueva lista',
       inputs: [
@@ -39,7 +38,9 @@ export class Tab1Page {
             if (data.titulo.length === 0) {
               return;
             } else {
-              this.deseosService.crearLista(data.titulo);
+              const listaId = this.deseosService.crearLista(data.titulo);
+              this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+
             }
           }
         }
