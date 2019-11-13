4d6ecd29843ca8651c34e00d2b3227c2323ec1a9
Alex Ballera <alexballera@gmail.com>
2019-11-12 23:03:05 -0300

    Agregado Localstorage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,14 +9,28 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-    const lista1 = new Lista('Terminar curso Angular');
-    const lista2 = new Lista('Volverme Front End');
+    this.cargarStorage();
+    // const lista1 = new Lista('Terminar curso Angular');
+    // const lista2 = new Lista('Volverme Front End');
 
-    this.listas.push(lista1, lista2);
+    // this.listas.push(lista1, lista2);
   }
 
   crearLista(titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+    if (localStorage.getItem('data')) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
   }
 }
